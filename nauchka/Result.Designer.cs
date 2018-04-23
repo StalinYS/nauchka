352c3e7712fdf14e2155223ab8c9ca6ea75219ae
alina mir <alya-1998@list.ru>
2018-04-23 16:00:25 +0300

    initial project version

---
+++ nauchka/Result.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace nauchka
+{
+    partial class Result
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataSet1 = new System.Data.DataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(541, 228);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // Result
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 294);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Result";
+            this.Text = "Result";
+            this.Load += new System.EventHandler(this.Result_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Data.DataSet dataSet1;
+    }
+}
---
+++ nauchka/Result.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace nauchka
+{
+    public partial class Result : Form
+    {
+
+        string n, f;
+        public Result(string file,string num)
+        {
+            InitializeComponent();
+            n = num;
+            f = file;
+        }
+
+        DataTable lecturerData;
+        private void CreateLecturerData()
+        {
+            lecturerData = new DataTable();
+            lecturerData.Columns.Add("number", typeof(Int32));
+            lecturerData.Columns.Add("topic", typeof(String));
+            lecturerData.Columns.Add("type", typeof(String));
+            lecturerData.Columns.Add("date", typeof(String));
+            lecturerData.Columns.Add("time", typeof(String));
+            lecturerData.Columns.Add("hoursAmount", typeof(Int32));
+            lecturerData.Columns.Add("signature", typeof(String));
+
+        }
+        private void LoadData()
+        {
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("http://timetable.sbmt.by/shedule/lecturer/" + f);
+
+
+            XmlNodeList elemList = doc.GetElementsByTagName("lesson");
+            for (int i = 0; i < elemList.Count; i++)
+            {
+                DataRow tempRow = lecturerData.NewRow();
+
+                tempRow["type"] = elemList[i]["type"].InnerText;
+                tempRow["date"] = elemList[i]["date"].InnerText;
+                tempRow["time"] = elemList[i]["time"].InnerText;
+                lecturerData.Rows.Add(tempRow);
+            }
+
+        }
+
+
+        private void Result_Load(object sender, EventArgs e)
+        {
+            label1.Text = n;
+            CreateLecturerData();
+            try
+            {
+                LoadData();
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось загрузить данные преподавателей");
+            }
+            dataGridView1.DataSource = lecturerData;
+            dataGridView1.Columns["number"].HeaderText = "№";
+            dataGridView1.Columns["number"].Width = 100;
+            dataGridView1.Columns["topic"].HeaderText = "Тема";
+            dataGridView1.Columns["topic"].Width = 250;
+            dataGridView1.Columns["type"].HeaderText = "Тип";
+            dataGridView1.Columns["type"].Width = 200;
+            dataGridView1.Columns["date"].HeaderText = "Дата";
+            dataGridView1.Columns["date"].Width = 150;
+            dataGridView1.Columns["time"].HeaderText = "Время";
+            dataGridView1.Columns["time"].Width = 150;
+
+
+
+            //dataSet1.ReadXml("http://timetable.sbmt.by/shedule/lecturer/" + f);
+
+            //dataGridView1.DataSource = dataSet1;
+
+
+            //DataTable lecturerTable = dataSet1.Tables["lesson"];
+
+            //foreach (DataRow workRow in lecturerData.Rows)
+            //{
+
+            //    DataRow tempRow = lecturerTable.NewRow();
+            //    tempRow["type"] = workRow["type"];
+            //    tempRow["date"] = workRow["date"];
+            //    tempRow["time"] = workRow["time"];
+
+            //    lecturerTable.Rows.Add(tempRow);
+            //}
+
+        }
+        }
+    }
+
